e189067ed4a6ec9a0d87fa38369f3be9e8c797dc
sx-motive <sx.motive@gmail.com>
2022-03-18 14:05:28 +0500

    fix issues

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -7,11 +7,11 @@ export default function About() {
       <div className="container -right">
         <div className="about-desc">
           <Paragraph>
-            Over the lasts years I realized - It's not enough to simply sell
-            products anymore, people need brands that lead from the heart and
-            share their values. That's why I focus on Branding. Whether a brand
-            is looking for a refresh or finding its voice at the very begging, I
-            will help find its purpose and connect with their target.
+            Over the lasts years I realized - It&apos;s not enough to simply
+            sell products anymore, people need brands that lead from the heart
+            and share their values. That&apos;s why I focus on Branding. Whether
+            a brand is looking for a refresh or finding its voice at the very
+            begging, I will help find its purpose and connect with their target.
           </Paragraph>
           <Paragraph>
             I believe in creating beautiful and complete brand experiences by
@@ -19,13 +19,13 @@ export default function About() {
             communicate their purpose.
           </Paragraph>
           <Paragraph>
-            Present Currently I'm working at Walsh and also as a freelancer
+            Present Currently I&apos;m working at Walsh and also as a freelancer
             looking for new brands to create complete branding experiences and
             help them find their voice.
           </Paragraph>
           <Paragraph>
             I believe collaboration allows us to create a different narratives
-            for each brand. That's why depending on the project I love
+            for each brand. That&apos;s why depending on the project I love
             collaborating with specialists from different fields
           </Paragraph>
         </div>
